b4480fb76fd1e4628108a63dc8cfdcd2e05a3a3f
Herbivore <arandlemiller97@yahoo.com>
2026-03-22 20:50:20 -0500

    mu is passing

--- statistics/normal.py
+++ statistics/normal.py
@@ -1,4 +1,10 @@
 def Mu(size, prob):
+	if (size < 1):
+		raise Exception("Size must be a non-negative number and greater than 0")
+
+	if (prob > 1 or prob < 0):
+		raise Exception("Probabliity should be a non-negative value and 0 <= p <= 1")
+
 	return size * prob
 
 def Sigma(mu, prob):
