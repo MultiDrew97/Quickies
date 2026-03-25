ef0cf3d3766e4cb962621127ea3c7bce2ce072d8
Herbivore <arandlemiller97@yahoo.com>
2026-03-25 18:32:03 -0500

    added the ressurect function and added a contion for checking if the entity can be revived or killed, defaulting to True in both

--- gol/entity.py
+++ gol/entity.py
@@ -3,12 +3,33 @@ from gol import Status
 
 class Entity:
 	def __init__(self, name="New Entity"):
-		print("Creating Entity...")
+		print(f"{name} is gestating...")
 		self.__name__ = name
 		self.__status__ = Status.ALIVE
 
 	def is_alive(self):
 		return self.__status__ != Status.DEAD
 
-	def kill(self) -> None:
+	def can_kill(self) -> bool:
+		# Determines whether the entity can be set to dead
+		return True
+
+	def kill(self) -> bool:
+		print(f"Attempting to kill {self.__name__}...")
+		if not self.can_kill():
+			raise Exception(f"{self.__name__} cannot die 👎🏾")
+
 		self.__status__ = Status.DEAD
+		return not self.is_alive()
+
+	def can_revive(self) -> bool:
+		# Determines whether the entity can be set to alive again
+		return True
+
+	def ressurect(self) -> bool:
+		print(f"Attempting to revive {self.__name__}")
+		if not self.can_revive():
+			raise Exception(f"{self.__name__} cannot be revived. GG brother 😅")
+
+		self.__status__ = Status.ALIVE
+		return self.is_alive()
--- gol/test/test_entity.py
+++ gol/test/test_entity.py
@@ -7,5 +7,12 @@ def test_kill():
 	entity = Entity()
 
 	assert entity.is_alive()
-	entity.kill()
-	assert not entity.is_alive()
+	assert entity.kill()
+
+def test_ressurect():
+	entity = Entity()
+
+	if entity.is_alive():
+		entity.kill()
+		
+	assert entity.ressurect()
