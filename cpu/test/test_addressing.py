7d078c82930de707cf1771db9805dc9af7a4765a
Herbivore <arandlemiller97@yahoo.com>
2026-05-11 11:45:36 -0500

    continuing work on implementing addressing modes and more opcodes for the unit
    updated tests to have more cases

--- cpu/test/test_addressing.py
+++ cpu/test/test_addressing.py
@@ -1,9 +1,7 @@
 
 from cpu import OP_CODES, Memory
-from cpu.addressing import zero_page, zero_page_indexed
+from cpu.addressing import indirect, zero_page, zero_page_indexed
 from cpu.unit import CPU
-
-cpu = CPU()
 
 def test_zp():
 	value = 0x0
@@ -18,7 +16,7 @@ def test_zp():
 	assert zero_page(0xFF) == 0xFF
 	assert zero_page(0x01) == 0x01
 
-	with cpu:
+	with CPU() as cpu:
 		cpu.execute(mem)
 		assert cpu.__read__(zero_page(loc), mem=mem) == value
 
@@ -42,11 +40,25 @@ def test_zp_indexed():
 		0x000B: index + 1
 	}
 
-	assert zero_page_indexed(index, x_value) == 0x18
-	assert zero_page_indexed(index, y_value) == 0x28
+	assert zero_page_indexed(index, register=x_value) == 0x18
+	assert zero_page_indexed(index, register=y_value) == 0x28
 
-	with cpu:
+	with CPU() as cpu:
 		cpu.execute(mem)
 		assert cpu.__read__(zero_page_indexed(index, register=x_value), mem=mem) == value
 		assert cpu.__read__(zero_page_indexed(index, register=y_value), mem=mem) == x_value
 		assert cpu.__read__(zero_page_indexed(index + 1, register=x_value), mem=mem) == y_value
+
+def test_indirect():
+	lo = 0x34
+	hi = 0x12
+	value = 0x69
+	mem = {
+		0x1234: value
+	}
+	assert indirect(lo, hi) == 0x1234
+
+	with CPU() as cpu:
+		cpu.__write__(lo, 0x0000, mem=mem)
+		cpu.__write__(hi, 0x0001, mem=mem)
+		assert cpu.__read__(indirect(lo, hi), mem=mem) == value
---
+++ cpu/test/test_unit.py
@@ -0,0 +1,109 @@
+from cpu import FLAGS, MEMORY_MIN_ADDRESS, NULL_POINTER, OP_CODES
+from cpu.unit import CPU
+
+def test_init():
+	tmp = CPU()
+	assert len(tmp.flags) == 8 and all(flag == False for flag in tmp.flags.values())
+
+def test_reset():
+	loc = 0x1234
+	value = 0x69
+	mem = {
+		0x1234: OP_CODES.LDA_IM.value,
+		0x1235: value
+	}
+	with CPU() as cpu:
+		cpu.PC = loc
+		cpu.execute(mem)
+		assert cpu.PC != loc
+		assert cpu.A == value
+
+		cpu.reset()
+
+		assert cpu.PC == MEMORY_MIN_ADDRESS
+		assert cpu.SP == cpu.A == cpu.X == cpu.Y == NULL_POINTER
+		print(cpu.flags.values())
+		assert len(cpu.flags) == 8 and all(flag is False for flag in cpu.flags.values())
+
+def test_negative_flag():
+	with CPU() as cpu:
+		cpu.__set_a__(0xFF)
+		assert cpu.flags[FLAGS.N] == True
+
+		cpu.__set_a__(0x7F)
+		assert cpu.flags[FLAGS.N] == False
+
+def test_zero_flag():
+	with CPU() as cpu:
+		cpu.__set_a__(0x0)
+		assert cpu.flags[FLAGS.Z] == True
+
+		cpu.__set_a__(0xFF)
+		assert cpu.flags[FLAGS.Z] == False
+
+def test_overflow_flag():
+	with CPU() as cpu:
+		assert cpu.flags[FLAGS.V] == False
+
+def test_set_a():
+	with CPU() as cpu:
+		cpu.__set_a__(0x0)
+		assert cpu.A == 0x0
+		assert cpu.flags[FLAGS.Z] == True
+		assert cpu.flags[FLAGS.N] == False
+
+		cpu.__set_a__(0xFF)
+		assert cpu.A == 0xFF
+		assert cpu.flags[FLAGS.Z] == False
+		assert cpu.flags[FLAGS.N] == True
+
+		cpu.__set_a__(0x7F)
+		assert cpu.A == 0x7F
+		assert cpu.flags[FLAGS.Z] == False
+		assert cpu.flags[FLAGS.N] == False
+
+def test_set_x():
+	with CPU() as cpu:
+		cpu.__set_x__(0x0)
+		assert cpu.X == 0x0
+		assert cpu.flags[FLAGS.Z] == True
+		assert cpu.flags[FLAGS.N] == False
+
+		cpu.__set_x__(0xFF)
+		assert cpu.X == 0xFF
+		assert cpu.flags[FLAGS.Z] == False
+		assert cpu.flags[FLAGS.N] == True
+
+		cpu.__set_x__(0x7F)
+		assert cpu.X == 0x7F
+		assert cpu.flags[FLAGS.Z] == False
+		assert cpu.flags[FLAGS.N] == False
+
+def test_set_y():
+	with CPU() as cpu:
+		cpu.__set_y__(0x0)
+		assert cpu.Y == 0x0
+		assert cpu.flags[FLAGS.Z] == True
+		assert cpu.flags[FLAGS.N] == False
+
+		cpu.__set_y__(0xFF)
+		assert cpu.Y == 0xFF
+		assert cpu.flags[FLAGS.Z] == False
+		assert cpu.flags[FLAGS.N] == True
+
+		cpu.__set_y__(0x7F)
+		assert cpu.Y == 0x7F
+		assert cpu.flags[FLAGS.Z] == False
+		assert cpu.flags[FLAGS.N] == False
+
+def test_fetch():
+	start = 0x0000
+	value = 0x69
+	mem = {
+		0x0000: value
+	}
+
+	with CPU() as cpu:
+		cpu.PC = start
+		assert cpu.fetch(mem) == value
+		assert cpu.PC == start + 1
